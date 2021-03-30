7a1f39e7fcf3db3b65fa903f5ee90b810fcf7660
Tom Fream <tomfream@gmail.com>
2021-03-30 16:47:07 -0600

    assertEqual problem

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (const index in firstArray) {
+    if (firstArray[index] !== secondArray[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  const assertionPassed = `😘 😘 😘 Assertion Passed: ${actual} === ${expected}`;
+  const assertionFaied = `🤬 🤬 🤬 Assertion failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(assertionPassed);
+  } else {
+    console.log(assertionFaied);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
