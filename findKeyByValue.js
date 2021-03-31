452bdca0a68a523fe671e072a0e4cf56361bc72d
Tom Fream <tomfream@gmail.com>
2021-03-31 16:55:48 -0600

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  const assertionPassed = `😘 😘 😘 Assertion Passed: ${actual} === ${expected}`;
+  const assertionFaied = `😰 😰 😰 Assertion failed: ${actual} !== ${expected}`;
+  if (actual === expected) {
+    console.log(assertionPassed);
+  } else {
+    console.log(assertionFaied);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  objectKeys = Object.keys(object);
+  for (const key of objectKeys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
