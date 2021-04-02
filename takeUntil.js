e46e032a25561de3da8f3b7e796ec7eede0c5706
Tom Fream <tomfream@gmail.com>
2021-04-01 19:21:17 -0600

    takeUntil lotide problem

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+// Assert functions
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (const index in firstArray) {
+    if (firstArray[index] !== secondArray[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const assertionPassed = `😘 😘 😘 Array Assertion Passed: ${actual} === ${expected}`;
+  const assertionFaied = `😰 😰 😰 Array Assertion failed: ${actual} !== ${expected}`;
+  
+  if (eqArrays(actual, expected)) {
+    return  assertionPassed;
+  }
+  return assertionFaied;
+};
+
+// takeUntil function implementation
+const takeUntil = function(array, callback) {
+ const returnArr = [];
+ for (const element of array)
+  if (!callback(element)){
+    returnArr.push(element);
+  } else break;
+  return returnArr;
+}
+
+// Test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
