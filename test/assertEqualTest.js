8e815e58901d0291698a82071ab2e403e5e643e8
Tom Fream <tomfream@gmail.com>
2021-04-05 16:22:53 -0600

    test files

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+console.log(assertEqual(1,2));
+console.log(assertEqual(1, 1));
